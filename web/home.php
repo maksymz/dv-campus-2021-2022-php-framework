24f8b424bf467a1f0077020aa281aad6f735d900
{DV.Campus} <maksimz@default-value.com>
2021-10-02 12:29:10 +0300

    #2 Shop Skeleton: implemented basic routing for catalog and products

--- web/home.php
+++ web/home.php
@@ -39,6 +39,7 @@
     </header>
 
     <main>
+        <!-- @TODO: Implement recently viewed products -->
         <section title="Recently Viewed Products">
             <h2>Recently Viewed Products</h2>
             <div class="product-list">
