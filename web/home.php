e499b515d58f9b0b61642b86d6e47bd965e8e9fa
{DV.Campus} <maksimz@default-value.com>
2021-10-02 11:41:11 +0300

    #2 Shop Skeleton: added data.php for categories and products; implemented top menu

--- web/home.php
+++ web/home.php
@@ -1,3 +1,6 @@
+<?php
+    require_once 'data.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,12 +27,15 @@
         <a href="/" title="{DV.Campus} PHP Framework">
             <img src="logo.jpg" alt="{DV.Campus} Logo" width="200"/>
         </a>
-        <menu>
-            <li>Category 1</li>
-            <li>Category 2</li>
-            <li>Category 3</li>
-            <li>Category 4</li>
-        </menu>
+        <nav>
+            <ul>
+                <?php foreach (catalogGetCategory() as $category) : ?>
+                    <li>
+                        <a href="/<?= $category['url'] ?>"><?= $category['name'] ?></a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </nav>
     </header>
 
     <main>
