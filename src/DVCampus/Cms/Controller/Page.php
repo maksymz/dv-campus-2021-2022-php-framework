1d8b792bb241ccc33e02f84660895fa0176ef10c
{DV.Campus} <dv.campus.pc@gmail.com>
2021-10-09 12:16:37 +0300

    #3 MVC: Added more CMS test pages

--- src/DVCampus/Cms/Controller/Page.php
+++ src/DVCampus/Cms/Controller/Page.php
@@ -6,9 +6,20 @@ namespace DVCampus\Cms\Controller;
 
 class Page implements \DVCampus\Framework\Http\ControllerInterface
 {
+    private \DVCampus\Framework\Http\Request $request;
+
+    /**
+     * @param \DVCampus\Framework\Http\Request $request
+     */
+    public function __construct(
+        \DVCampus\Framework\Http\Request $request
+    ) {
+        $this->request = $request;
+    }
+
     public function execute(): string
     {
-        $page = 'home.php';
+        $page = $this->request->getParameter('page') . '.php';
 
         ob_start();
         require_once "../src/page.php";
--- src/DVCampus/Cms/Router.php
+++ src/DVCampus/Cms/Router.php
@@ -8,12 +8,31 @@ use DVCampus\Cms\Controller\Page;
 
 class Router implements \DVCampus\Framework\Http\RouterInterface
 {
+    private \DVCampus\Framework\Http\Request $request;
+
+    /**
+     * @param \DVCampus\Framework\Http\Request $request
+     */
+    public function __construct(
+        \DVCampus\Framework\Http\Request $request
+    ) {
+        $this->request = $request;
+    }
+
     /**
      * @inheritDoc
      */
     public function match(string $requestUrl): string
     {
-        if ($requestUrl === '') {
+        $cmsPage = [
+            '',
+            'test-page',
+            'test-page-2'
+        ];
+
+        if (in_array($requestUrl, $cmsPage)) {
+            $this->request->setParameter('page', $requestUrl ?: 'home');
+
             return Page::class;
         }
 
